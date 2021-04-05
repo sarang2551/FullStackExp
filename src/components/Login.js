cbe403d139393b0108c3816e94338ac116bf67ce
sarang2551 <74551805+sarang2551@users.noreply.github.com>
2021-04-05 18:10:51 +0800

    Some minor changes for Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,49 +1,115 @@
 import React from "react";
+import { Redirect } from "react-router-dom";
 import "../login.scss";
+import Axios from "axios";
+import ErrorComponent from "./error";
 export default class LoginBox extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      username: "",
+      password: "",
+      resData: {},
+      redirect: "",
+      errorProps: {},
+      renderLoginMessage: false
+    };
   }
-
+  handleChange = (e) => {
+    e.preventDefault();
+    const { name, value } = e.target;
+    this.setState({ ...this.state, [name]: value });
+  };
   submitLogin(e) {
     e.preventDefault();
+    var userInfo = {
+      username: this.state.username,
+      password: this.state.password
+    };
+    console.log(userInfo);
+    Axios.post("http://localhost:3000/login", userInfo).then((res) => {
+      let loginProps = null;
+      const { username, loginStatus } = res.data;
+      console.log("res.data ", res.data);
+      switch (loginStatus) {
+        case "success":
+          this.setState({ redirect: "/mainpage", resData: res.data });
+          return;
+        case "failed":
+          loginProps = {
+            type: "failed",
+            content: `${username} login failed. Incorrect password!`
+          };
+          this.setState({
+            errorProps: loginProps,
+            renderLoginMessage: true
+          });
+          return;
+        case "notFound":
+          loginProps = { type: "warn", content: `User could not be found!` };
+          this.setState({
+            errorProps: loginProps,
+            renderLoginMessage: true
+          });
+          return;
+        default:
+          return <div></div>;
+      }
+    });
   }
 
   render() {
+    if (this.state.redirect) {
+      return (
+        <Redirect
+          to={{ pathname: this.state.redirect, state: this.state.resData }}
+        />
+      );
+    }
     return (
-      <div className="inner-container">
-        <div className="header">Login</div>
-        <div className="box">
-          <div className="input-group">
-            <label htmlFor="username">Username</label>
-            <input
-              type="text"
-              name="username"
-              className="login-input"
-              placeholder="Username"
-            />
-          </div>
+      <>
+        <div className="inner-container">
+          <div className="header">Login</div>
+          <div className="box">
+            <div className="input-group">
+              <label htmlFor="username">Username</label>
+              <input
+                required
+                type="text"
+                name="username"
+                className="login-input"
+                placeholder="Username"
+                onChange={this.handleChange}
+              />
+            </div>
 
-          <div className="input-group">
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              name="password"
-              className="login-input"
-              placeholder="Password"
-            />
-          </div>
+            <div className="input-group">
+              <label htmlFor="password">Password</label>
+              <input
+                required
+                type="password"
+                name="password"
+                className="login-input"
+                placeholder="Password"
+                onChange={this.handleChange}
+              />
+            </div>
 
-          <button
-            type="button"
-            className="login-btn"
-            onClick={this.submitLogin.bind(this)}
-          >
-            Login
-          </button>
+            <button
+              type="button"
+              className="login-btn"
+              onClick={this.submitLogin.bind(this)}
+            >
+              Login
+            </button>
+          </div>
         </div>
-      </div>
+        {this.state.renderLoginMessage ? (
+          <>
+            <ErrorComponent errorProps={this.state.errorProps} />
+          </>
+        ) : null}
+      </>
     );
   }
 }
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,58 +1,114 @@
 import React from "react";
 import "../login.scss";
+import Axios from "axios";
+import ErrorComponent from "./error";
 export default class RegisterBox extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      username: "",
+      password: "",
+      email: "",
+      isShowErrorComponent: false,
+      errorProps: {}
+    };
   }
-
+  handleChange = (e) => {
+    e.preventDefault();
+    const { name, value } = e.target;
+    this.setState({ ...this.state, [name]: value });
+  };
   submitRegister(e) {
     e.preventDefault();
+    var userInfo = {
+      username: this.state.username,
+      email: this.state.email,
+      password: this.state.password
+    };
+    console.log(userInfo);
+    Axios.post("http://localhost:3000/register", userInfo).then((res) => {
+      console.log("res.data", res.data);
+      const { username, success } = res.data;
+      let errorProps = null;
+
+      if (success === "registered") {
+        errorProps = {
+          type: "success",
+          content: `${username} is successfully registered!`
+        };
+      }
+
+      if (success === "failed") {
+        errorProps = {
+          type: "error",
+          content: `Failed to registered ${username}`
+        };
+      }
+
+      if (success === "duplicate") {
+        errorProps = {
+          type: "warn",
+          content: `${username} already exists`
+        };
+      }
+
+      this.setState({ errorProps: errorProps, isShowErrorComponent: true });
+    });
   }
 
   render() {
     return (
-      <div className="inner-container">
-        <div className="header">Register</div>
-        <div className="box">
-          <div className="input-group">
-            <label htmlFor="username">Username</label>
-            <input
-              type="text"
-              name="username"
-              className="login-input"
-              placeholder="Username"
-            />
-          </div>
+      <>
+        <div className="inner-container">
+          <div className="header">Register</div>
+          <div className="box">
+            <div className="input-group">
+              <label htmlFor="username">Username</label>
+              <input
+                type="text"
+                name="username"
+                className="login-input"
+                placeholder="Username"
+                onChange={this.handleChange}
+              />
+            </div>
 
-          <div className="input-group">
-            <label htmlFor="email">Email</label>
-            <input
-              type="text"
-              name="email"
-              className="login-input"
-              placeholder="Email"
-            />
-          </div>
+            <div className="input-group">
+              <label htmlFor="email">Email</label>
+              <input
+                type="text"
+                name="email"
+                className="login-input"
+                placeholder="Email"
+                onChange={this.handleChange}
+              />
+            </div>
 
-          <div className="input-group">
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              name="password"
-              className="login-input"
-              placeholder="Password"
-            />
+            <div className="input-group">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                name="password"
+                className="login-input"
+                placeholder="Password"
+                onChange={this.handleChange}
+              />
+            </div>
+            <button
+              type="button"
+              className="login-btn"
+              onClick={this.submitRegister.bind(this)}
+            >
+              Register
+            </button>
           </div>
-          <button
-            type="button"
-            className="login-btn"
-            onClick={this.submitRegister.bind(this)}
-          >
-            Register
-          </button>
         </div>
-      </div>
+        {this.state.isShowErrorComponent ? (
+          <>
+            <ErrorComponent errorProps={this.state.errorProps} />
+          </>
+        ) : null}
+      </>
     );
   }
 }
